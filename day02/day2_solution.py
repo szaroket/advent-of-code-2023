71e1ad4f901ce58d482f12d92064d32a40429e2b
szaroket <30178095+szaroket@users.noreply.github.com>
2023-12-02 21:19:16 +0100

    Add solution to part 2 of Day 2

--- day02/day2_solution.py
+++ day02/day2_solution.py
@@ -2,6 +2,7 @@
 # Day 2: Cube Conundrum
 # https://adventofcode.com/2023/day/2
 import re
+from collections import defaultdict
 
 CUBES_IN_BAG = {
     "red": 12,
@@ -16,9 +17,9 @@ def read_input() -> list[str]:
     return data
 
 
-if __name__ == "__main__":
+def solution_1() -> None:
     input_data = read_input()
-    sum = 0
+    cubes_sum = 0
     for line in input_data:
         splitted_line = line.replace(" ", "").split(":")
         game_number = int(re.findall(r"\d+", splitted_line[0])[0])
@@ -32,7 +33,42 @@ if __name__ == "__main__":
                 correct_result += 1
 
         if correct_result == number_of_cubes:
-            sum += int(game_number)
-            print(f"Game {game_number} is possible!")
+            cubes_sum += int(game_number)
 
-    print(f"Final result is {sum}.")
+    print(f"Part 2: final result it {cubes_sum}")
+
+
+def solution_2() -> None:
+    input_data = read_input()
+    result = 0
+    for line in input_data:
+        splitted_line = line.replace(" ", "").split(":")
+        cubes_sets = splitted_line[1].replace(";", "").replace(",", "")
+        iter_cubes = iter(list(filter(None, re.split(r'(\d+)', cubes_sets))))
+        list_of_cubes = list(zip(iter_cubes, iter_cubes))
+        cubes_dict = defaultdict(list)
+        for number, color in list_of_cubes:
+            cubes_dict[color].append(int(number))
+
+        green_cubes = 0
+        red_cubes = 0
+        blue_cubes = 0
+        cubes_sum = 0
+
+        for red_number in cubes_dict["red"]:
+            for blue_number in cubes_dict["blue"]:
+                for green_number in cubes_dict["green"]:
+                    if red_number + blue_number + green_number > cubes_sum:
+                        cubes_sum = red_number + blue_number + green_number
+                        green_cubes = green_number
+                        red_cubes = red_number
+                        blue_cubes = blue_number
+
+        multiple = red_cubes * green_cubes * blue_cubes
+        result += multiple
+    print(f"Part 2: final result it {result}")
+
+
+if __name__ == "__main__":
+    solution_1()
+    solution_2()
