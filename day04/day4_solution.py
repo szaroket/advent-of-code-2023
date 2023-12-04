00faea9e44979f1731d421e7fff990e28f7582c6
szaroket <30178095+szaroket@users.noreply.github.com>
2023-12-04 20:06:36 +0100

    Add solution to part 2 of Day 4

--- day04/day4_solution.py
+++ day04/day4_solution.py
@@ -4,7 +4,7 @@
 
 
 import re
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 def read_input() -> list[str]:
@@ -16,7 +16,9 @@ def read_input() -> list[str]:
 if __name__ == "__main__":
     input_data = read_input()
     result = 0
-    for line in input_data:
+    cards = defaultdict(int)
+    for card_number, line in enumerate(input_data, 1):
+        cards[card_number] += 1
         all_numbers = re.sub(r"Card +\d+:", "", line)
         all_numbers_list = [
             int(number) for number in re.findall(r"\d+", all_numbers)
@@ -28,5 +30,15 @@ if __name__ == "__main__":
         points = 0
         if how_many_winning_numbers != 0:
             points = pow(2, how_many_winning_numbers - 1)
+            number_of_operations = cards[card_number]
+            for operation in range(number_of_operations):
+                for idx in range(
+                    card_number + 1, card_number + 1 + how_many_winning_numbers
+                ):
+                    cards[idx] += 1
         result += points
+
     print(f"Part 1: result it {result}.")
+
+    results_2 = sum(cards.values())
+    print(f"Part 2: result it {results_2}.")
